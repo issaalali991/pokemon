4f7399eaf1d5dcb3d9b782875af1e8a247030099
workingj <workingj@pm.me>
2024-02-09 17:37:16 +0100

    aösdlkfjdlk

--- client/src/components/Pokemon.jsx
+++ client/src/components/Pokemon.jsx
@@ -1,48 +1,55 @@
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../contexts/PokemonContext";
+import PokemonDefault from "./PokemonDefault";
 
 export default function Pokemon() {
   const { pokeList } = useContext(DataContext);
-  const pokemon=Math.floor(Math.random()*pokeList.length);
-  
-  return (
+  const pokemon = Math.floor(Math.random() * pokeList.length);
+  const [selected, setSelected] = useState(false);
+
+  return !selected ? (
+    <div className="Pokemon">
+      <PokemonDefault
+        selectHandler={() => {
+          setSelected(true);
+          console.log("TEST");
+        }}
+      />
+    </div>
+  ) : (
     <div className="Pokemon">
       <h3>Pokemon</h3>
-      <PokeImage pokemon= {pokeList[pokemon]} />
-      <PokeData pokemon= {pokeList[pokemon]}
-      />
+      <PokeImage pokemon={pokeList[pokemon]} />
+      <PokeData pokemon={pokeList[pokemon]} />
     </div>
   );
 }
 
-function PokeData(
-  {pokemon}
-) {
-  return <div className="PokeData">
-    <div className="bg-green-500 text-white font-bold rounded-full h-12  flex items-center justify-center"
-    >
-     HP: {pokemon.base.HP}
-    </div>
-    <div>
-      <h2 className="text-2xl font-bold text-gray-800 mb-2 mt-2 text-center "
-      >{pokemon.name.english}</h2>
-      <ul className="grid grid-cols-2 gap-2 text-gray-800 "
-      >
-        <li>Type: {pokemon.type.join(", ")}</li>
-        <li>S-Attack: {pokemon.base["Sp. Attack"]}</li>
-        <li>S-Defense: {pokemon.base["Sp. Defense"]}</li>
+function PokeData({ pokemon }) {
+  return (
+    <div className="PokeData">
+      <div className="bg-green-500 text-white font-bold rounded-full h-12  flex items-center justify-center">
+        HP: {pokemon.base.HP}
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-gray-800 mb-2 mt-2 text-center ">
+          {pokemon.name.english}
+        </h2>
+        <ul className="grid grid-cols-2 gap-2 text-gray-800 ">
+          <li>Type: {pokemon.type.join(", ")}</li>
+          <li>S-Attack: {pokemon.base["Sp. Attack"]}</li>
+          <li>S-Defense: {pokemon.base["Sp. Defense"]}</li>
 
-        <li>Attack: {pokemon.base.Attack}</li>
-        <li>Defense: {pokemon.base.Defense}</li>
-        <li>Speed: {pokemon.base.Speed}</li>
-        
-      </ul>
+          <li>Attack: {pokemon.base.Attack}</li>
+          <li>Defense: {pokemon.base.Defense}</li>
+          <li>Speed: {pokemon.base.Speed}</li>
+        </ul>
+      </div>
     </div>
-  </div>;
-  
+  );
 }
 
-function PokeImage({pokemon}) {
+function PokeImage({ pokemon }) {
   const [pokeImg, setPokeImg] = useState(null);
 
   useEffect(() => {
@@ -55,13 +62,15 @@ function PokeImage({pokemon}) {
       setPokeImg(data.sprites.front_default);
     };
     getApiData();
-  }
-  );
-  return <div >
-    <img src={pokeImg} alt={pokemon.name.english} 
-    className="w-48 h-48 mx-auto object-cover rounded-full border-4 border-green-500 w-48 h-48 mx-auto object-cover rounded-full border-4 border-green-500
+  });
+  return (
+    <div className="PokeImage">
+      <img
+        src={pokeImg}
+        alt={pokemon.name.english}
+        className="w-48 h-48 mx-auto object-cover rounded-full border-4 border-green-500 w-48 h-48 mx-auto object-cover rounded-full border-4 border-green-500
     "
-    />
-    
-  </div>;
+      />
+    </div>
+  );
 }
---
+++ client/src/components/PokemonDefault.jsx
@@ -0,0 +1,8 @@
+
+export default function PokemonDefault({selectHandler}) {
+  return (
+    <div className="pcontainter">
+      <div className="DefaultImage" onClick={selectHandler}></div>
+    </div>
+  );
+}
