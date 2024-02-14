bcd42b3dda32fade4a5554905036527266657e16
Fara <f_karamizadeh@yahoo.com>
2024-02-14 18:47:19 +0100

    data path and Tialwind config

--- client/src/components/FightButton.jsx
+++ client/src/components/FightButton.jsx
@@ -4,7 +4,7 @@ function FightButton() {
   return (
     <div>
       <button>
-        <img src="../public/fight.svg" alt="" className="mb-4 w-72 h-72" />
+        <img src="/fight.svg" alt="" className="mb-4 w-72 h-72" />
       </button>
     </div>
   );
